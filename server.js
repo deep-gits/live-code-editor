ffac9d9261ca2063a9f069782d412d6647da0964
deep-gits <deepaksaraswat8913@gmail.com>
2025-12-06 23:12:30 +0530

    fixed

--- server.js
+++ server.js
@@ -6,33 +6,35 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+// 1. Serve the index.html file
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
+// 2. Handle WebSocket Connections
 io.on('connection', (socket) => {
     console.log('User connected: ' + socket.id);
 
-    // 1. Handle Code Changes
+    // FEATURE A: Code Synchronization
+    // When a user types code, send it to everyone ELSE
     socket.on('code-change', (code) => {
         socket.broadcast.emit('code-update', code);
     });
 
-    // 2. Handle Chat Messages (NEW)
+    // FEATURE B: Chat Messages
+    // When a user sends a msg, send it to EVERYONE (including sender)
     socket.on('chat-message', (msg) => {
-        // Send the message to EVERYONE (including the sender)
         io.emit('chat-message', msg);
     });
 
+    // Handle Disconnect
     socket.on('disconnect', () => {
         console.log('User disconnected');
     });
 });
 
-server.listen(3000, () => {
-    console.log('Server running on http://localhost:3000');
-});
-
+// 3. Start the Server (Fixing the Error)
+// Render uses process.env.PORT. Localhost uses 3000.
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
